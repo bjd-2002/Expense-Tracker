12271ed25e26fc301c4d0aae063433c74f8e91a1
bjd-2002 <ee21b030@smail.iitm.ac.in>
2024-04-19 20:05:38 +0530

    minor fixes

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -9,7 +9,7 @@ export const TransactionList = () => {
   return (
     <>
       <h3>History</h3>
-      <ul className="list">
+      <ul >
         {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
       </ul>
     </>
